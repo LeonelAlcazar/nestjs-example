e77e2ab966d95dca5374810664e54fdc09a980d7
YLi Alex <alex@hks.ar>
2024-11-19 14:11:14 -0300

    fix types

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsNumber, IsNumberString, IsOptional } from 'class-validator';
 
 export class PaginationDTO {
   @IsNumber()
@@ -7,7 +7,7 @@ export class PaginationDTO {
   @ApiProperty()
   page: number;
 
-  @IsNumber()
+  @IsNumberString()
   @IsOptional()
   @ApiProperty()
   limit: number;
